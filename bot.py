cf1e456fabbe2e0ab7bb73f49730c7bdf562b006
anlu408 <71849385+anlu408@users.noreply.github.com>
2020-09-29 14:19:26 -0700

    Working on Commands.
    Moved Events to Commands.py

--- bot.py
+++ bot.py
@@ -5,21 +5,6 @@ from discord.ext import commands
 #Sets the commands set in discord chat to start with a . i.e. .help
 client = commands.Bot(command_prefix = '.')
 
-@client.event
-#When the bot is ready with info from discord it is put into a ready state
-async def on_ready():
-    print('Bot is ready.')
-
-@client.event
-#When a new member joins the server their entrance is announced.
-async def on_member_join(member):
-    print(f'{member} has joined the server.')
-
-@client.event
-#When a member leaves or is kicked from the server their exit is announced.
-async def on_member_remove(member):
-    print(f'{member} has left the server')
-
 """
 The lines of code below import any other Python file found in Cogs.
 At the moment it just includes the basic bot.py along with the
